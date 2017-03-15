905fe5881dc3db5f0eb74cb4b4277e6ad7ab4a05
David Woodruff <david_woodruff@byu.edu>
2017-03-15 17:56:18 -0600

    Add tests to the lifecycle phases

--- test/lifecycle/check-test.js
+++ test/lifecycle/check-test.js
@@ -1,2 +1,75 @@
 const accountConfig = require('../../lib/util/account-config')(`${__dirname}/../test-account-config.yml`).getAccountConfig();
 const checkPhase = require('../../lib/lifecycle/check');
+const EnvironmentContext = require('../../lib/datatypes/environment-context');
+const ServiceContext = require('../../lib/datatypes/service-context');
+const expect = require('chai').expect;
+
+function getServiceDeployers() {
+    return {
+        ecs: {
+            check: function(serviceContext) {
+                return [];
+            }
+        },
+        efs: {
+            check: function(serviceContext) {
+                return [];
+            }
+        }
+    }
+}
+
+function getEnvironmentContext() {
+    //Construct EnvironmentContext
+    let appName = "FakeApp"
+    let deployVersion = "1";
+    let environmentName = "dev";
+    let environmentContext = new EnvironmentContext(appName, deployVersion, environmentName);
+
+    //Construct ServiceContext A
+    let serviceNameA = "A";
+    let serviceTypeA = "ecs";
+    let paramsA = {
+        some: "param"
+    }
+    let serviceContextA = new ServiceContext(appName, environmentName, serviceNameA, serviceTypeA, deployVersion, paramsA);
+    environmentContext.serviceContexts[serviceNameA] = serviceContextA;
+
+    //Construct ServiceContext B
+    let serviceNameB = "B";
+    let serviceTypeB = "efs"
+    let paramsB = {
+        other: "param"
+    }
+    let serviceContextB = new ServiceContext(appName, environmentName, serviceNameB, serviceTypeB, deployVersion, paramsB);
+    environmentContext.serviceContexts[serviceNameB] = serviceContextB;
+    return environmentContext;
+}
+
+describe('check', function() {
+    describe('checkServices', function() {
+        it('should run check services on all services in the environment', function() {
+            let serviceDeployers = getServiceDeployers();
+            let environmentContext = getEnvironmentContext();    
+
+            let checkResults = checkPhase.checkServices(serviceDeployers, environmentContext);
+            expect(checkResults).to.deep.equal([]);
+        });
+        
+        it('should return errors when there are errors in one or more services', function() {
+            let serviceDeployers = getServiceDeployers();
+            let ecsErrors = ['ECS Error'];
+            serviceDeployers['ecs'].check = function() {
+                return ecsErrors
+            }
+            let efsErrors = ['EFS Errors'];
+            serviceDeployers['efs'].check = function() {
+                return efsErrors;
+            }
+            let environmentContext = getEnvironmentContext();    
+
+            let checkResults = checkPhase.checkServices(serviceDeployers, environmentContext);
+            expect(checkResults).to.deep.equal(ecsErrors.concat(efsErrors));
+        });
+    });
+});
--- test/lifecycle/deploy-test.js
+++ test/lifecycle/deploy-test.js
@@ -1,2 +1,73 @@
 const accountConfig = require('../../lib/util/account-config')(`${__dirname}/../test-account-config.yml`).getAccountConfig();
 const deployPhase = require('../../lib/lifecycle/deploy');
+const EnvironmentContext = require('../../lib/datatypes/environment-context');
+const ServiceContext = require('../../lib/datatypes/service-context');
+const PreDeployContext = require('../../lib/datatypes/pre-deploy-context');
+const DeployContext = require('../../lib/datatypes/deploy-context');
+const expect = require('chai').expect;
+
+describe('deploy', function() {
+    describe('deployServicesInLevel', function() {
+        it('should deploy the services in the given level', function() {
+            let serviceDeployers = {
+                efs: {
+                    deploy: function(toDeployServiceContext, toDeployPreDeployContext, dependenciesDeployContexts) {
+                        throw new Error("Should not have called ECS in this level");
+                    }
+                },
+                ecs: {
+                    deploy: function(toDeployServiceContext, toDeployPreDeployContext, dependenciesDeployContexts) {
+                        return Promise.resolve(new DeployContext(toDeployServiceContext));
+                    }
+                }
+            }
+
+            //Create EnvironmentContext
+            let appName = "test";
+            let deployVersion = "1";
+            let environmentName = "dev";
+            let environmentContext = new EnvironmentContext(appName, deployVersion, environmentName);
+
+
+            //Construct ServiceContext B
+            let serviceNameB = "B";
+            let serviceTypeB = "efs"
+            let paramsB = {
+                other: "param"
+            }
+            let serviceContextB = new ServiceContext(appName, environmentName, serviceNameB, serviceTypeB, deployVersion, paramsB);
+            environmentContext.serviceContexts[serviceNameB] = serviceContextB;
+
+            //Construct ServiceContext A
+            let serviceNameA = "A";
+            let serviceTypeA = "ecs";
+            let paramsA = {
+                some: "param",
+                dependencies: [ serviceNameB]
+            }
+            let serviceContextA = new ServiceContext(appName, environmentName, serviceNameA, serviceTypeA, deployVersion, paramsA);
+            environmentContext.serviceContexts[serviceNameA] = serviceContextA;
+
+            //Construct PreDeployContexts
+            let preDeployContexts = {}
+            preDeployContexts[serviceNameA] = new PreDeployContext(serviceContextA);
+            preDeployContexts[serviceNameB] = new PreDeployContext(serviceContextB);
+
+            //Construct DeployContexts 
+            let deployContexts = {}
+            deployContexts[serviceNameB] = new DeployContext(serviceContextB);
+
+            //Set deploy order 
+            let deployOrder = [
+                [serviceNameB],
+                [serviceNameA]
+            ]
+            let levelToDeploy = 1;
+
+            return deployPhase.deployServicesInLevel(serviceDeployers, environmentContext, preDeployContexts, deployContexts, deployOrder, levelToDeploy)
+                .then(deployContexts => {
+                    expect(deployContexts[serviceNameA]).to.be.instanceOf(DeployContext);
+                });
+        });
+    });
+});
