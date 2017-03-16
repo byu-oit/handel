8cecca6d8a24de5f49fa4e9a21eedd59855666f9
David Woodruff <david_woodruff@byu.edu>
2017-03-16 11:39:48 -0600

    Comment out Beanstalk since I haven't really done anything with it yet

--- test/services/beanstalk/beanstalk-test.js
+++
@@ -1,2 +0,0 @@
-const accountConfig = require('../../../lib/util/account-config')(`${__dirname}/../../test-account-config.yml`).getAccountConfig();
-const beanstalk = require('../../../lib/services/beanstalk');
