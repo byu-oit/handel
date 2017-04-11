05a9125877d1cc535ed7db8bfb31f8a424c6d3fc
David Woodruff <david_woodruff@byu.edu>
2017-04-11 15:44:01 -0600

    Initial SNS topic support (no events yet)

--- test/services/sns/sns-test.js
+++ test/services/sns/sns-test.js
@@ -1,5 +1,5 @@
 const accountConfig = require('../../../lib/util/account-config')(`${__dirname}/../../test-account-config.yml`).getAccountConfig();
-const sqs = require('../../../lib/services/sqs');
+const sns = require('../../../lib/services/sns');
 const cloudfFormationCalls = require('../../../lib/aws/cloudformation-calls');
 const ServiceContext = require('../../../lib/datatypes/service-context');
 const DeployContext = require('../../../lib/datatypes/deploy-context');
@@ -8,7 +8,7 @@ const BindContext = require('../../../lib/datatypes/bind-context');
 const sinon = require('sinon');
 const expect = require('chai').expect;
 
-describe('sqs deployer', function() {
+describe('sns deployer', function() {
     let sandbox;
 
     beforeEach(function() {
@@ -22,7 +22,7 @@ describe('sqs deployer', function() {
     describe('check', function() {
         it('shouldnt validate anything yet', function() {
             let serviceContext = new ServiceContext("FakeApp", "FakeEnv", "FakeService", "FakeType", "1", {});
-            let errors = sqs.check(serviceContext);
+            let errors = sns.check(serviceContext);
             expect(errors).to.deep.equal([]);
         });
     });
@@ -30,7 +30,7 @@ describe('sqs deployer', function() {
     describe('preDeploy', function() {
         it('should return an empty predeploy context since it doesnt do anything', function() {
             let serviceContext = new ServiceContext("FakeApp", "FakeEnv", "FakeService", "FakeType", "1", {});
-            return sqs.preDeploy(serviceContext)
+            return sns.preDeploy(serviceContext)
                 .then(preDeployContext => {
                     expect(preDeployContext).to.be.instanceof(PreDeployContext);
                     expect(preDeployContext.appName).to.equal(serviceContext.appName);
@@ -41,7 +41,7 @@ describe('sqs deployer', function() {
     describe('bind', function() {
         it('should return an empty bind context since it doesnt do anything', function() {
             let serviceContext = new ServiceContext("FakeApp", "FakeEnv", "FakeService", "FakeType", "1", {});
-            return sqs.bind(serviceContext)
+            return sns.bind(serviceContext)
                 .then(bindContext => {
                     expect(bindContext).to.be.instanceof(BindContext);
                     expect(bindContext.appName).to.equal(serviceContext.appName);
@@ -50,145 +50,119 @@ describe('sqs deployer', function() {
     });
 
     describe('deploy', function() {
-        it('should create a new queue when the stack doesnt exist', function() {
+        it('should create a new topic when one doesnt exist', function() {
             let appName = "FakeApp";
             let envName = "FakeEnv";
             let serviceName = "FakeService";
-            let serviceType = "sqs";
-            let queueName = "FakeQueue";
-            let queueArn = "FakeArn";
-            let queueUrl = "FakeUrl";
+            let serviceType = "sns";
+            let topicName = "FakeTopic";
+            let topicArn = "FakeArn";
 
             let getStackStub = sandbox.stub(cloudfFormationCalls, 'getStack').returns(Promise.resolve(null));
             let createStackStub = sandbox.stub(cloudfFormationCalls, 'createStack').returns(Promise.resolve({
                 Outputs: [
                     {
-                        OutputKey: 'QueueName',
-                        OutputValue: queueName
+                        OutputKey: 'TopicName',
+                        OutputValue: topicName
                     },
                     {
-                        OutputKey: 'QueueArn',
-                        OutputValue: queueArn
-                    },
-                    {
-                        OutputKey: 'QueueUrl',
-                        OutputValue: queueUrl
+                        OutputKey: 'TopicArn',
+                        OutputValue: topicArn
                     }
                 ]
             }));
 
             let ownServiceContext = new ServiceContext(appName, envName, serviceName, serviceType, "1", {
-                type: 'sqs',
-                queue_type: 'fifo',
-                content_based_deduplication: true,
-                delay_seconds: 2,
-                max_message_size: 262140,
-                message_retention_period: 345601,
-                visibility_timeout: 40
+                type: 'sns'
             });
             let ownPreDeployContext = new PreDeployContext(ownServiceContext);
 
-            return sqs.deploy(ownServiceContext, ownPreDeployContext, [])
+            return sns.deploy(ownServiceContext, ownPreDeployContext, [])
                 .then(deployContext => {
                     expect(getStackStub.calledOnce).to.be.true;
                     expect(createStackStub.calledOnce).to.be.true;
 
                     expect(deployContext).to.be.instanceof(DeployContext);
 
-                    //Should have exported 3 env vars
-                    let queueNameEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_NAME`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueNameEnv]).to.equal(queueName);
-                    let queueUrlEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_URL`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueUrlEnv]).to.equal(queueUrl);
-                    let queueArnEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_ARN`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueArnEnv]).to.equal(queueArn);
+                    //Should have exported 2 env vars
+                    let topicNameEnv = `${serviceType}_${appName}_${envName}_${serviceName}_TOPIC_NAME`.toUpperCase()
+                    expect(deployContext.environmentVariables[topicNameEnv]).to.equal(topicName);
+                    let topicArnEnv = `${serviceType}_${appName}_${envName}_${serviceName}_TOPIC_ARN`.toUpperCase()
+                    expect(deployContext.environmentVariables[topicArnEnv]).to.equal(topicArn);
 
                     //Should have exported 1 policy
                     expect(deployContext.policies.length).to.equal(1); //Should have exported one policy
-                    expect(deployContext.policies[0].Resource[0]).to.equal(queueArn);
+                    expect(deployContext.policies[0].Resource[0]).to.equal(topicArn);
                 });
         });
 
-        it('should update the stack when the queue already exists', function() {
+        it('should update the topic if it already exists', function() {
             let appName = "FakeApp";
             let envName = "FakeEnv";
             let serviceName = "FakeService";
-            let serviceType = "sqs";
-            let queueName = "FakeQueue";
-            let queueArn = "FakeArn";
-            let queueUrl = "FakeUrl";
+            let serviceType = "sns";
+            let topicName = "FakeTopic";
+            let topicArn = "FakeArn";
 
             let getStackStub = sandbox.stub(cloudfFormationCalls, 'getStack').returns(Promise.resolve({}));
             let updateStackStub = sandbox.stub(cloudfFormationCalls, 'updateStack').returns(Promise.resolve({
                 Outputs: [
                     {
-                        OutputKey: 'QueueName',
-                        OutputValue: queueName
+                        OutputKey: 'TopicName',
+                        OutputValue: topicName
                     },
                     {
-                        OutputKey: 'QueueArn',
-                        OutputValue: queueArn
-                    },
-                    {
-                        OutputKey: 'QueueUrl',
-                        OutputValue: queueUrl
+                        OutputKey: 'TopicArn',
+                        OutputValue: topicArn
                     }
                 ]
             }));
 
             let ownServiceContext = new ServiceContext(appName, envName, serviceName, serviceType, "1", {
-                type: 'sqs',
-                queue_type: 'fifo',
-                content_based_deduplication: true,
-                delay_seconds: 2,
-                max_message_size: 262140,
-                message_retention_period: 345601,
-                visibility_timeout: 40
+                type: 'sns'
             });
             let ownPreDeployContext = new PreDeployContext(ownServiceContext);
 
-            return sqs.deploy(ownServiceContext, ownPreDeployContext, [])
+            return sns.deploy(ownServiceContext, ownPreDeployContext, [])
                 .then(deployContext => {
                     expect(getStackStub.calledOnce).to.be.true;
                     expect(updateStackStub.calledOnce).to.be.true;
 
                     expect(deployContext).to.be.instanceof(DeployContext);
 
-                    //Should have exported 3 env vars
-                    let queueNameEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_NAME`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueNameEnv]).to.equal(queueName);
-                    let queueUrlEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_URL`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueUrlEnv]).to.equal(queueUrl);
-                    let queueArnEnv = `${serviceType}_${appName}_${envName}_${serviceName}_QUEUE_ARN`.toUpperCase()
-                    expect(deployContext.environmentVariables[queueArnEnv]).to.equal(queueArn);
+                    //Should have exported 2 env vars
+                    let topicNameEnv = `${serviceType}_${appName}_${envName}_${serviceName}_TOPIC_NAME`.toUpperCase()
+                    expect(deployContext.environmentVariables[topicNameEnv]).to.equal(topicName);
+                    let topicArnEnv = `${serviceType}_${appName}_${envName}_${serviceName}_TOPIC_ARN`.toUpperCase()
+                    expect(deployContext.environmentVariables[topicArnEnv]).to.equal(topicArn);
 
                     //Should have exported 1 policy
                     expect(deployContext.policies.length).to.equal(1); //Should have exported one policy
-                    expect(deployContext.policies[0].Resource[0]).to.equal(queueArn);
+                    expect(deployContext.policies[0].Resource[0]).to.equal(topicArn);
                 });
         });
     });
 
     describe('consumerEvents', function() {
-        it('should throw an error because SQS cant consume event services', function() {
-            return sqs.consumeEvents(null, null, null, null)
+        it('should throw an error because SNS cant consume event services', function() {
+            return sns.consumeEvents(null, null, null, null)
                 .then(consumeEventsContext => {
                     expect(true).to.be.false; //Shouldnt get here
                 })
                 .catch(err => {
-                    expect(err.message).to.contain("SQS service doesn't consume events");
+                    expect(err.message).to.contain("SNS service doesn't consume events");
                 });
         });
     });
 
     describe('produceEvents', function() {
-        it('should throw an error because SQS cant produce events for other services', function() {
-            return sqs.produceEvents(null, null, null, null)
+        it('should throw an error because SNS cant produce events for other services', function() {
+            return sns.produceEvents(null, null, null, null)
                 .then(produceEventsContext => {
                     expect(true).to.be.false; //Shouldnt get here
                 })
                 .catch(err => {
-                    expect(err.message).to.contain("SQS service doesn't produce events");
+                    expect(err.message).to.contain("SNS service doesn't produce events");
                 });
         });
     });
