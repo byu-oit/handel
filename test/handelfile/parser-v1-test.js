4907c91ae96a624db90a76b468dd8d65ae6e4b33
David Woodruff <david_woodruff@byu.edu>
2017-05-23 15:30:58 -0600

    Add license header to source files

--- test/handelfile/parser-v1-test.js
+++ test/handelfile/parser-v1-test.js
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 const accountConfig = require('../../lib/util/account-config')(`${__dirname}/../test-account-config.yml`).getAccountConfig();
 const parserV1 = require('../../lib/handelfile/parser-v1');
-const yaml = require('js-yaml');
-const fs = require('fs');
 const expect = require('chai').expect;
 
 describe('parser-v1', function() {
