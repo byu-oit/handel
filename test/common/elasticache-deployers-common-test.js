bfdc652b1e797bfc83ff8ceec056485704910785
David Woodruff <david_woodruff@byu.edu>
2017-07-10 11:29:45 -0600

    Fix ElastiCache cluster naming bug
    CloudFormation was creating a new cluster on every deploy because
    of the random characters appended to the end of the cluster name.
    
    This change removes those random characters and just uses more of
    the app and service names.

---
+++ test/common/elasticache-deployers-common-test.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+const elasticacheDeployersCommon = require('../../lib/common/elasticache-deployers-common');
+const ServiceContext = require('../../lib/datatypes/service-context');
+const sinon = require('sinon');
+const expect = require('chai').expect;
+
+describe('elasticache deployers common module', function () {
+    let sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe('getClusterName', function () {
+        it('should return the shortened cluster name from the ServiceContext', function () {
+            let serviceContext = new ServiceContext("MyFakeAppWithALongNameWithManyCharacters", "MyLongEnvName", "MyLongishServiceName", "redis", "1", {});
+            let clusterName = elasticacheDeployersCommon.getClusterName(serviceContext);
+            expect(clusterName).to.equal("MyFakeApp-MyL-MyLong");
+        });
+    });
+});
