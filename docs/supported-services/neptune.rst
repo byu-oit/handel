2dd3d7a5792e3d756fdd2cd59614e2c721816cb1
David Woodruff <david_woodruff@byu.edu>
2018-07-27 20:03:44 -0600

    Initial working version

---
+++ docs/supported-services/neptune.rst
@@ -0,0 +1,114 @@
+.. _neptune:
+
+Neptune
+=======
+This page contains information about using the Neptune service in Handel. This service provides a Neptune graph database cluster.
+
+Service Limitations
+-------------------
+No Update Support
+~~~~~~~~~~~~~~~~~
+This service intentionally does not support updates. Once a database is created, certain updates to the database will cause a new database to be created and the old one deleted. In an effort to avoid unwanted data loss, we don't update this service automatically. You can still modify the database and parameter group manually in the AWS console.
+
+.. WARNING::
+
+    Make sure you know what you're doing when you modify your Neptune database in the AWS Console. Certain actions will cause database downtime, and some may even cause the database to be recreated.
+
+Parameters
+----------
+.. list-table::
+   :header-rows: 1
+
+   * - Parameter
+     - Type
+     - Required
+     - Default
+     - Description
+   * - type
+     - string
+     - Yes
+     - 
+     - This must always be *neptune* for this service type.
+   * - description
+     - string
+     - No
+     - 
+     - The description on the resources created for the cluster
+   * - instance_type
+     - string
+     - No
+     - db.r4.large
+     - The size of database instance to run. See `Neptune pricing <https://aws.amazon.com/neptune/pricing/>`_ for the allowed instance types.
+   * - cluster_size
+     - number
+     - No
+     - 1
+     - The number of instances (including the primary) to run in your cluster.
+   * - cluster_parameters
+     - map<string, string>
+     - No
+     - 
+     - A list of key/value Neptune cluster parameter group pairs to configure your cluster. You will need to look in the AWS Console to see the list of available cluster parameters for Neptune.
+   * - instance_parameters
+     - map<string, string>
+     - No
+     - 
+     - A list of key/value Neptune instance parameter group pairs to configure the instances in your cluster. You will need to look in the AWS Console to see the list of available instance parameters for Neptune.
+   * - tags
+     - :ref:`tagging-resources`
+     - No
+     - 
+     - Any tags you wish to apply to this Neptune cluster.
+     
+.. WARNING::
+
+    Be aware that Neptune clusters can be very expensive. A cluster with 3 *db.r4.2xlarge* instances in it will cost about about $3,000/month. Make sure you check how much you will be paying!
+
+
+Example Handel File
+-------------------
+
+.. code-block:: yaml
+
+    version: 1
+
+    name: neptune-test
+
+    environments:
+      dev:
+        database:
+          type: neptune
+          instance_type: db.r4.large
+          cluster_size: 3
+          cluster_parameters: # This is where you can set parameters that configure the cluster as a whole
+            neptune_enable_audit_log: 0
+          instance_parameters: # This is where you can set parameters that apply to each instance.
+            neptune_query_timeout: 120000
+          tags:
+            some: tag
+
+Depending on this service
+-------------------------
+The Neptune service outputs the following environment variables:
+
+.. list-table::
+   :header-rows: 1
+
+   * - Environment Variable
+     - Description
+   * - <SERVICE_NAME>_CLUSTER_ENDPOINT
+     - The address that you should use for writes to the database.
+   * - <SERVICE_NAME>_READ_ENDPOINT
+     - The address that you should use for reads to the database.
+   * - <SERVICE_NAME>_PORT
+     - The port on which the Neptune cluster instances are listening.
+
+See :ref:`environment-variable-names` for information about how the service name is included in the environment variable name.
+
+Events produced by this service
+-------------------------------
+The Neptune service does not produce events for other Handel services to consume.
+
+Events consumed by this service
+-------------------------------
+The Neptune service does not consume events from other Handel services.
---
+++ handel/test/services/neptune/neptune-test.ts
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2018 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import { expect } from 'chai';
+import {
+    AccountConfig,
+    BindContext,
+    DeployContext,
+    PreDeployContext,
+    ServiceContext,
+    ServiceType,
+    UnBindContext,
+    UnDeployContext,
+    UnPreDeployContext
+} from 'handel-extension-api';
+import { awsCalls, bindPhase, deletePhases, preDeployPhase } from 'handel-extension-support';
+import 'mocha';
+import * as sinon from 'sinon';
+import config from '../../../src/account-config/account-config';
+import * as neptune from '../../../src/services/neptune';
+import { NeptuneConfig } from '../../../src/services/neptune/config-types';
+import { STDLIB_PREFIX } from '../../../src/services/stdlib';
+
+describe('neptune deployer', () => {
+    let sandbox: sinon.SinonSandbox;
+    const appName = 'FakeApp';
+    const envName = 'FakeEnv';
+    let serviceContext: ServiceContext<NeptuneConfig>;
+    let serviceParams: NeptuneConfig;
+    let accountConfig: AccountConfig;
+
+    beforeEach(async () => {
+        accountConfig = await config(`${__dirname}/../../test-account-config.yml`);
+        sandbox = sinon.sandbox.create();
+        serviceParams = {
+            type: 'neptune'
+        };
+        serviceContext = new ServiceContext(appName, envName, 'FakeService', new ServiceType(STDLIB_PREFIX, 'neptune'), serviceParams, accountConfig);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    // At the moment, check only validates the JSON schema, so no tests here for that phase at the moment
+
+    describe('preDeploy', () => {
+        it('should create a security group', async () => {
+            const groupId = 'FakeSgGroupId';
+            const preDeployContext = new PreDeployContext(serviceContext);
+            preDeployContext.securityGroups.push({
+                GroupId: groupId
+            });
+            const createSgStub = sandbox.stub(preDeployPhase, 'preDeployCreateSecurityGroup')
+                .resolves(preDeployContext);
+
+            const retPreDeployContext = await neptune.preDeploy(serviceContext);
+            expect(retPreDeployContext).to.be.instanceof(PreDeployContext);
+            expect(retPreDeployContext.securityGroups.length).to.equal(1);
+            expect(retPreDeployContext.securityGroups[0].GroupId).to.equal(groupId);
+            expect(createSgStub.callCount).to.equal(1);
+        });
+    });
+
+    describe('bind', () => {
+        it('should add the source sg to its own sg as an ingress rule', async () => {
+            const dependencyServiceContext = new ServiceContext(appName, envName, 'FakeService', new ServiceType(STDLIB_PREFIX, 'neptune'), serviceParams, accountConfig);
+            const dependencyPreDeployContext = new PreDeployContext(dependencyServiceContext);
+            const dependentOfServiceContext = new ServiceContext(appName, envName, 'FakeService', new ServiceType(STDLIB_PREFIX, 'beanstalk'), {type: 'beanstalk'}, accountConfig);
+            const dependentOfPreDeployContext = new PreDeployContext(dependentOfServiceContext);
+            const bindSgStub = sandbox.stub(bindPhase, 'bindDependentSecurityGroup')
+                .resolves(new BindContext(dependencyServiceContext, dependentOfServiceContext));
+
+            const bindContext = await neptune.bind(dependencyServiceContext, dependencyPreDeployContext,
+                dependentOfServiceContext, dependentOfPreDeployContext);
+            expect(bindContext).to.be.instanceof(BindContext);
+            expect(bindSgStub.callCount).to.equal(1);
+        });
+    });
+
+    describe('deploy', () => {
+        const envPrefix = 'FAKESERVICE';
+        const clusterEndpoint = 'fakeaddress.amazonaws.com';
+        const databasePort = 3306;
+        const readEndpoint = 'fakeaddress2.amazonaws.com';
+        let ownPreDeployContext: PreDeployContext;
+        let dependenciesDeployContexts: DeployContext[];
+        const deployedStack = {
+            Outputs: [
+                {
+                    OutputKey: 'ClusterEndpoint',
+                    OutputValue: clusterEndpoint
+                },
+                {
+                    OutputKey: 'ClusterPort',
+                    OutputValue: databasePort
+                },
+                {
+                    OutputKey: 'ClusterReadEndpoint',
+                    OutputValue: readEndpoint
+                }
+            ]
+        };
+
+        beforeEach(() => {
+            ownPreDeployContext = new PreDeployContext(serviceContext);
+            ownPreDeployContext.securityGroups.push({
+                GroupId: 'FakeId'
+            });
+
+            dependenciesDeployContexts = [];
+        });
+
+        it('should create the cluster if it doesnt exist', async () => {
+            const getStackStub = sandbox.stub(awsCalls.cloudFormation, 'getStack').resolves(null);
+            const createStackStub = sandbox.stub(awsCalls.cloudFormation, 'createStack')
+                .resolves(deployedStack);
+
+            const deployContext = await neptune.deploy(serviceContext, ownPreDeployContext, dependenciesDeployContexts);
+            expect(getStackStub.callCount).to.equal(1);
+            expect(createStackStub.callCount).to.equal(1);
+            expect(deployContext).to.be.instanceof(DeployContext);
+            expect(deployContext.environmentVariables[`${envPrefix}_CLUSTER_ENDPOINT`]).to.equal(clusterEndpoint);
+            expect(deployContext.environmentVariables[`${envPrefix}_PORT`]).to.equal(databasePort);
+            expect(deployContext.environmentVariables[`${envPrefix}_READ_ENDPOINT`]).to.equal(readEndpoint);
+        });
+
+        it('should not update the database if it already exists', async () => {
+            const getStackStub = sandbox.stub(awsCalls.cloudFormation, 'getStack').resolves(deployedStack);
+            const updateStackStub = sandbox.stub(awsCalls.cloudFormation, 'updateStack').resolves(null);
+
+            const deployContext = await neptune.deploy(serviceContext, ownPreDeployContext, dependenciesDeployContexts);
+            expect(getStackStub.callCount).to.equal(1);
+            expect(updateStackStub.callCount).to.equal(0);
+            expect(deployContext).to.be.instanceof(DeployContext);
+            expect(deployContext.environmentVariables[`${envPrefix}_CLUSTER_ENDPOINT`]).to.equal(clusterEndpoint);
+            expect(deployContext.environmentVariables[`${envPrefix}_PORT`]).to.equal(databasePort);
+            expect(deployContext.environmentVariables[`${envPrefix}_READ_ENDPOINT`]).to.equal(readEndpoint);
+        });
+    });
+
+    describe('unPreDeploy', () => {
+        it('should delete the security group', async () => {
+            const unPreDeployStub = sandbox.stub(deletePhases, 'unPreDeploySecurityGroup')
+                .resolves(new UnPreDeployContext(serviceContext));
+
+            const unPreDeployContext = await neptune.unPreDeploy(serviceContext);
+            expect(unPreDeployContext).to.be.instanceof(UnPreDeployContext);
+            expect(unPreDeployStub.callCount).to.equal(1);
+        });
+    });
+
+    describe('unBind', () => {
+        it('should unbind the security group', async () => {
+            const unBindStub = sandbox.stub(deletePhases, 'unBindSecurityGroups')
+                .resolves(new UnBindContext(serviceContext));
+
+            const unBindContext = await neptune.unBind(serviceContext);
+            expect(unBindContext).to.be.instanceof(UnBindContext);
+            expect(unBindStub.callCount).to.equal(1);
+        });
+    });
+
+    describe('unDeploy', () => {
+        it('should undeploy the stack', async () => {
+            const unDeployStackStub = sandbox.stub(deletePhases, 'unDeployService')
+                .resolves(new UnDeployContext(serviceContext));
+
+            const unDeployContext = await neptune.unDeploy(serviceContext);
+            expect(unDeployContext).to.be.instanceof(UnDeployContext);
+            expect(unDeployStackStub.callCount).to.equal(1);
+        });
+    });
+});
